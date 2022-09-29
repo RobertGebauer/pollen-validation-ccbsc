17aebfe064ebb21915ded05797a73d475b1944e4
Robert Gebauer <robert.gebauer@gmx.de>
2022-09-29 11:21:33 +0200

    Bump 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='ccbsc-api',
     packages=find_packages(),
-    version='0.0.5',
+    version='1.0.0',
     description='Framework to apply new algorithms on images of pollen.',
     author='Robert Gebauer',
     url="https://github.com/RobertGebauer/pollen-validation-ccbsc",
