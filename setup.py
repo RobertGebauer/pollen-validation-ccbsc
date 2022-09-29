ffb5abc9ce831c7cddeee61ed6d79ef86aa08e1a
Robert Gebauer <robert.gebauer@gmx.de>
2022-09-29 11:20:51 +0200

    Removed library not needed anymore.

--- setup.py
+++ setup.py
@@ -2,14 +2,10 @@ from setuptools import find_packages, setup
 setup(
     name='ccbsc-api',
     packages=find_packages(),
-    version='0.0.4',
+    version='0.0.5',
     description='Framework to apply new algorithms on images of pollen.',
     author='Robert Gebauer',
     url="https://github.com/RobertGebauer/pollen-validation-ccbsc",
     license='MIT',
-    platforms=["any"],
-
-    install_requires= [
-        "pillow == 8.0.1"
-    ]
+    platforms=["any"]
 )
